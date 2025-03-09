88a5a9b2303a2dcda60deba405c1fb75ac00989a
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-03-09 16:05:28 +0300

    Going 3D. but there is a problem with loadin texture next i will fix it.

--- QuantaView/Main.cpp
+++ QuantaView/Main.cpp
@@ -14,6 +14,8 @@
 #include"EBO.h"
 #include"Texture.h"
 
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 int main()
 {
@@ -29,19 +31,24 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLfloat vertices[] = {
-		-0.5f, -0.5f,0.0f,  1.0f,0.0f,0.0f,   0.0f,0.0f,//Lower left corner
-		-0.5f, 0.5f, 0.0f,  0.0f,1.0f,0.0f,   0.0f,1.0f,//Upper left corner
-		0.5f, 0.5f, 0.0f,   0.0f,0.0f,1.0f,   1.0f,1.0f,//Upper right corner
-		0.5f, -0.5f, 0.0f,  1.0f,1.0f,0.0f,   1.0f,0.0f//Lower right corner
+	-0.5f, 0.0f,  0.5f,  0.83f, 0.70f, 0.44f,  0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,  0.83f, 0.70f, 0.44f,  0.0f, 1.0f,
+	 0.5f, 0.0f, -0.5f,  0.83f, 0.70f, 0.44f,  1.0f, 1.0f,
+	 0.5f, 0.0f,  0.5f,  0.83f, 0.70f, 0.44f,  1.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,  0.92f, 0.86f, 0.76f,  0.5f, 1.0f
 	};
 
 	GLuint indices[] = {
-		0,2,1, //Upper Triangle
-		0,3,2  //Lower Triangle
+		0,1,2,
+		0,2,3,
+		0,1,4,
+		1,2,4,
+		2,3,4,
+		3,0,4
 	};
 
 	//create a window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "QuantaView", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "QuantaView", NULL, NULL);
 
 	//GLFWimage images[1];
 	//images[0].pixels = stbi_load("C:/Users/HP/Desktop", &images[0].width, &images[0].height, 0, 4);
@@ -63,7 +70,7 @@ int main()
 	gladLoadGL();
 
 	//Specify the viewport of OpenGL in the Window
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 
 	Shader shaderProgram("default.vert", "default.frag");
@@ -88,6 +95,11 @@ int main()
 	Texture sampleTexture("Icon_Bird_512x512.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	sampleTexture.TexUnit(shaderProgram, "tex0", 0);
 
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
+
 	//Specify the color of the background
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -104,12 +116,39 @@ int main()
 		//And window resizing
 		//And many other things
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
+
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1/60)
+		{
+			rotation += 0.2f;
+			prevTime = crntTime;
+		}
+
+		//initialize the model matrix
+		glm::mat4 model = glm::mat4(1.0f);
+		//initialize the view matrix
+		glm::mat4 view = glm::mat4(1.0f);
+		//initialize the projection matrix
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);	
+
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+		
 		glUniform1f(uniID, 0.5f);
 		sampleTexture.Bind();
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
